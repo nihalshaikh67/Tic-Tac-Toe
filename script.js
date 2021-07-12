23a611d8a71d78f81831b6347be78df0a0147ce9
nihalshaikh67 <nihalshaikh834@gmail.com>
2021-07-13 01:20:16 +0530

    changed

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 let sign = "X";
 let display = document.getElementById("player");
 let block;
+let gameOver  = false;
 
+function printx(number) {
+    if(!gameOver){
 
-function printx(number){
-let block = document.getElementById("r"+number);
-console.log(block);
+        let block = document.getElementById("r" + number);
+        console.log(block);
+        
+        if (block.innerHTML == "") {
+            block.innerHTML = sign;
+            winner();
+          if(!gameOver){
 
-if(block.innerHTML == ""){
-block.innerHTML = sign;
-winner();
-checkSign();
-display.innerHTML = "<center>"+sign + "'s Turn now"+"</center>"
-
- }
+              checkSign();
+              display.innerHTML = "<center>" + sign + "'s Turn now" + "</center>"
+            }
+            
+        }
+    }
 
 
 }
-function checkSign(){
-    if(sign == "X")
-    sign = "O"
+function checkSign() {
+    if (sign == "X")
+        sign = "O"
     else
-    sign = "X";
-    
+        sign = "X";
+
 }
 
-function getBox(no){
-    return document.getElementById("r"+no).innerHTML;
+function getBox(no) {
+    return document.getElementById("r" + no).innerHTML;
 }
-function checkMove(a,b,c,m){
-    if(getBox(a)==m && getBox(b)==m && getBox(c)==m){
+function checkMove(a, b, c, m) {
+    if (getBox(a) == m && getBox(b) == m && getBox(c) == m) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function winner(){
-if(checkMove(1,2,3,sign) || checkMove(4,5,6,sign) ||  checkMove(4,5,6,sign) || checkMove(7,8,9,sign) ||  checkMove(1,4,7,sign) ||  checkMove(2,5,8,"X") ||  checkMove(3,6,9,sign) ||  checkMove(1,5,9,sign) ||  checkMove(3,5,7,sign))
-        {
-          
-            display.innerHTML = "<center>Congratulations ! "+sign + " Won the game" + "</center>";
-            alert(sign +" has won the game");
-            // setTimeout(clr(),10000);
-            throw "End Game";
-        
+function winner() {
+    if (checkMove(1, 2, 3, sign) || checkMove(4, 5, 6, sign) || checkMove(4, 5, 6, sign) || checkMove(7, 8, 9, sign) || checkMove(1, 4, 7, sign) || checkMove(2, 5, 8, sign) || checkMove(3, 6, 9, sign) || checkMove(1, 5, 9, sign) || checkMove(3, 5, 7, sign)) {
 
-    }else{
-        if(getBox(1) != "" && getBox(2) != "" && getBox(3) != "" && getBox(4) != "" && getBox(5) != "" && getBox(6) != "" && getBox(7) != "" && getBox(8) != "" && getBox(9) != ""){
-            display.innerHTML = "<center>Its a Tie !</center>"; 
-            throw "ITS A TIE";
+        display.innerHTML = "<center>Congratulations ! " + sign + " Won the game" + "</center>";
+        alert(sign + " has won the game");
+        // setTimeout(clr(),10000);
+        gameOver = true;
+        return "End Game";
+
+
+    } else {
+        if (getBox(1) != "" && getBox(2) != "" && getBox(3) != "" && getBox(4) != "" && getBox(5) != "" && getBox(6) != "" && getBox(7) != "" && getBox(8) != "" && getBox(9) != "") {
+            display.innerHTML = "<center>Its a Tie !</center>";
+            return "ITS A TIE";
         }
     }
 }
-function clr(){
+function clr() {
     display.innerHTML = "<center>Lets Play</center>"
     // for(let i = 1 ; i <=9;i++)
     //  {
@@ -61,4 +67,3 @@ function clr(){
     // }
     location.reload();
 }
-
